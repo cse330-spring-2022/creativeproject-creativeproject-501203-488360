3aefb1977a380ce5b48e49442b4c6baa8af9f138
Christopher Patrick <cpatrick67135@gmail.com>
2022-04-20 11:29:47 -0500

    Register button now leads to student dashboard

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -3,12 +3,16 @@ import { Routes, Route } from 'react-router-dom';
 
 import Register from './pages/Register';
 import Login from './pages/Login';
+import StudentDashboard from './pages/StudentDashboard';
+import ProfessorDashboard from './pages/ProfessorDashboard';
 
 const Main = () => {
   return (
     <Routes> {/* The Switch decides which component to show based on the current URL.*/}
       <Route exact path='/' element={<Register />}></Route>
-      <Route exact path='/login' component={<Login />}></Route>
+      <Route exact path='/login' element={<Login />}></Route>
+      <Route exact path='/studentdashboard' element={<StudentDashboard />}></Route>
+      <Route exact path='/professordashboard' element={<ProfessorDashboard />}></Route>
     </Routes>
   );
 }
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,19 +1,60 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+// import { useNavigate } from "react-router-dom";
 
-class Register extends Component {
-    render() {
-        return (
-            <>
-                <div>
-                    Register
-                </div>
-                <button>
-                    Register
-                </button>
-            </>
-        );
+// class Register extends Component {
+//     constructor(props){
+//         super(props);
+//         this.register = this.register.bind(this);
+//     }
+//     register(){
+//         const navigate = useNavigate();
+//         navigate("/studentdashboard");
+//     }
+//     render() {
+//         return (
+//             <>
+//                 <div>
+//                     Register
+//                 </div>
+//                 <button>
+//                     Register
+//                 </button>
+//             </>
+//         );
+//     }
+// }
+
+
+// export default Register;
+
+
+import React, { Component } from 'react';
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from 'react';
+// import PropTypes from 'prop-types';
+
+function App(props) {
+    const navigate = useNavigate();
+    function Register(){
+        navigate("/studentdashboard");
     }
+
+    return (
+        <>
+            <div>
+                Register
+            </div>
+            <button onClick={Register}>
+                Register
+            </button>
+        </>
+    );
 }
 
 
-export default Register;
+
+// Register.propTypes = {
+
+// };
+
+export default App;
